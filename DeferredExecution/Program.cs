46d7df055822b86987e562f7a50840edf1a21cbd
mamba <joao7yt@gmail.com>
2024-06-21 14:50:10 -0300

    fix: typos

--- DeferredExecution/Program.cs
+++ DeferredExecution/Program.cs
@@ -18,21 +18,21 @@ internal class DeferredExecution
 
         Console.WriteLine($"Example 1");
         Console.WriteLine();
-        Console.WriteLine($"We will create 2 lists of strings, one using IEnumerable<string>");
+        Console.WriteLine($"We will create 2 collections of strings, one using IEnumerable<string>");
         Console.WriteLine($"and the other using List<string>.");
         Console.WriteLine();
-        Console.WriteLine($"Both lists will contain a single string: DateTime.Now.ToList()");
+        Console.WriteLine($"Both collections will contain a single string: DateTime.Now.ToList()");
         Console.WriteLine();
         Console.WriteLine($"Both IEnumerable<> and List<> were created now, at the same time,");
         Console.WriteLine($"the List<> was created this way:");
         Console.WriteLine($"    list = iEnumerable.ToList();");
         Console.WriteLine();
-        Console.WriteLine($"So, by intuition, we would assume values from both lists would");
+        Console.WriteLine($"So, by intuition, we would assume values from both collections would");
         Console.WriteLine($"be the same.");
         Console.WriteLine();
-        Console.WriteLine($"This is the DateTime of right after both lists were created: {DateTime.Now}");
+        Console.WriteLine($"This is the DateTime of right after both collections were created: {DateTime.Now}");
         Console.WriteLine();
-        Console.WriteLine($"Press any key to show the values of both lists.");
+        Console.WriteLine($"Press any key to show the values of both collections.");
 
         Console.ReadKey();
         Console.WriteLine();
@@ -47,8 +47,8 @@ internal class DeferredExecution
         Console.WriteLine($"IEnumerable<> is enumerated, that is, when they are accessed. In our");
         Console.WriteLine($"example they were accessed to write them to the console.");
         Console.WriteLine();
-        Console.WriteLine($"You can force this to happen earlier by calling IEnumerable<>.ToList().");
-        Console.WriteLine($"That's why the list using List<> got the DateTime.Now from right when");
+        Console.WriteLine($"We can force this to happen earlier by calling IEnumerable<>.ToList().");
+        Console.WriteLine($"That's why the collection using List<> got the DateTime.Now from right when");
         Console.WriteLine($"it was created, because it was created using: list = iEnumerable.ToList(),");
         Console.WriteLine($"which enumerated the IEnumerable<>.");
         Console.WriteLine();
@@ -77,7 +77,7 @@ internal class DeferredExecution
         Console.WriteLine($"previous example, the List<> was created as follows:");
         Console.WriteLine($"    list = iEnumerable.ToList();");
         Console.WriteLine();
-        Console.WriteLine($"By intuition, we would assume values from both lists would");
+        Console.WriteLine($"By intuition, we would assume values from both collections would");
         Console.WriteLine($"be the same.");
         Console.WriteLine();
         Console.WriteLine($"Let's take a look:");
@@ -104,7 +104,7 @@ internal class DeferredExecution
 
         Console.WriteLine($"Example 3");
         Console.WriteLine();
-        Console.WriteLine($"We will use Random() again, this time passing the iEnumerable list");
+        Console.WriteLine($"We will use Random() again, this time passing the iEnumerable collection");
         Console.WriteLine($"as parameter to methods in another class. We will use 2 methods, one");
         Console.WriteLine($"receiving a List<int> and the other receiving an IEnumerable<int>.");
         Console.WriteLine();
@@ -120,15 +120,15 @@ internal class DeferredExecution
         OtherClass.ListMethod(iEnumerable.ToList());
         Console.WriteLine();
 
-        Console.WriteLine($"As you can see, every time we enumrate the IEnumerable<>, a new random");
+        Console.WriteLine($"As we can see, every time we enumrate the IEnumerable<>, a new random");
         Console.WriteLine($"number is generated. Surely it can be useful in some specific situations,");
-        Console.WriteLine($"but it can cause a big headache if you implement it this way without knowing");
+        Console.WriteLine($"but it can cause a big headache if we implement it this way without knowing");
         Console.WriteLine($"about this behavior and end up causing bugs because of it.");
         Console.WriteLine();
         Console.WriteLine($"Now let's force enumeration on the IEnumerable<>.");
         Console.WriteLine();
         Console.WriteLine($"We could do this from the begining by using .ToList(), even if we are not");
-        Console.WriteLine($"dealing with a List<> list:");
+        Console.WriteLine($"dealing with a List<> collection:");
         Console.WriteLine($"    var iEnumerable = iterations.Select(x => rnd.Next(0, 1000)).ToList();");
         Console.WriteLine();
         Console.WriteLine($"But as the iEnumerable is already created, we will force its enumeration");
